417acb3bb8587144c69bb784627348c5f8b2fcfa
xiongleon2020 <129012304+xiongleon2020@users.noreply.github.com>
2023-04-21 17:44:18 +0800

    Update session.py
    update session headers

--- wencai/core/session.py
+++ wencai/core/session.py
@@ -12,7 +12,8 @@ class Session(requests.Session):
         'Connection': 'keep-alive',
         'Content-Type': "application/x-www-form-urlencoded; charset=UTF-8",
         'User-Agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
-        'X-Requested-With': "XMLHttpRequest"
+        'X-Requested-With': "XMLHttpRequest",
+        'Host': 'www.iwencai.com'
 
     }
 
